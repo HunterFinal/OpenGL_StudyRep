a752f36fe3a4e9353aa6e5224d16ba505b6772f8
麦 <139850260+HunterFinal@users.noreply.github.com>
2025-11-27 23:12:01 +0900

    [OpenGL]Back up
    OpenGL pdf page 34

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,25 @@
 #include "glad/glad.h"
 #include "glfw3/glfw3.h"
 
+namespace OpenGLStudy
+{
+  struct GL_NDCVertex
+  {
+    union
+    {
+      struct
+      {
+        float X;
+        float Y;
+        float Z;  
+      };
+
+      float XYZ[3];
+    };
+  };
+}
+
+
 namespace
 {
   static const char* WINDOW_TITLE = "LearnOpenGL";
@@ -17,8 +36,19 @@ namespace
   static void processInput(GLFWwindow*);
 }
 
+namespace GLSLShaderText
+{
+  static const char* vertexShaderSource =
+  "#version 460 core\nlayout (location = 0) in vec3 aPos;\nvoid main()\n{\n  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}\0";
+
+  static const char* fragmentShaderSource =
+  "#version 460 core\nout vec4 FragColor;\nvoid main()\n{\n  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}\0";
+}
+
 int main()
 {
+  using OpenGLStudy::GL_NDCVertex;
+
   std::print("Hello GCC!");
 
   glfwInit();
@@ -51,6 +81,102 @@ int main()
   
       glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
+
+      GL_NDCVertex vertices[] =
+      {
+        {-0.5f, -0.5f, 0.0f},
+        { 0.5f, -0.5f, 0.0f},
+        { 0.0f,  0.5f, 0.0f}
+      };
+
+      // Use vertex buffer object to store vertices(OpenGL object)
+      uint32_t VBO = 0;
+      glGenBuffers(1, &VBO);
+      glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+      /**
+       * Copy user-defined data into the currently bound buffer
+       * First argument: Type of buffer
+       * Second argument: Size of the data we want to pass to the buffer
+       * Third argument: Actual data we want to send
+       * Fourth argument: Specifies how we want the graphics card to manage the given data
+       *                  GL_STREAM_DRAW: The data is set only once and used by the GPU at most a few times
+       *                  GL_STATIC_DRAW: The data is set only once and used many times
+       *                  GL_DYNAMIC_DRAW: The data is changed a lot and used many times
+       */
+      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+      {
+
+      }
+      constexpr int32_t infoLogLength = 512;
+      // Create vertex shader object(OpenGL object)
+      uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+      /**
+       * First argument: Shader object to compile
+       * Second argument: Specifies how many strings we're passing as source code
+       * Third argument: Source code of GLSL
+       * Fourth argument: We can leave it to NULL //NOTE Figure out why
+       */
+      glShaderSource(vertexShader, 1, &GLSLShaderText::vertexShaderSource, nullptr);
+      glCompileShader(vertexShader);
+
+      // Check compilation status
+      {
+        int32_t success;
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+          char infoLog[infoLogLength];
+          glGetShaderInfoLog(vertexShader, infoLogLength, nullptr, infoLog);
+          std::println("ERROR:Shader vertex compilation failed\n{}", infoLog);
+        }
+      }
+      
+      // Create fragment shader object(OpenGL object)
+      uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+      glShaderSource(fragmentShader, 1, &GLSLShaderText::fragmentShaderSource, nullptr);
+      glCompileShader(fragmentShader);
+
+      // Check compilation status
+      {
+        int32_t success;
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+          char infoLog[infoLogLength];
+          glGetShaderInfoLog(fragmentShader, infoLogLength, nullptr, infoLog);
+          std::println("ERROR:Shader fragment compilation failed\n{}", infoLog);
+        }
+      }
+
+      // Link both vertex and fragment to a shader program object(OpenGL object)
+      uint32_t shaderProgram;
+      shaderProgram = glCreateProgram();
+      glAttachShader(shaderProgram, vertexShader);
+      glAttachShader(shaderProgram, fragmentShader);
+      glLinkProgram(shaderProgram);
+    
+      // Check link status
+      {
+        int32_t success;
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+          char infoLog[infoLogLength];
+          glGetProgramInfoLog(shaderProgram, infoLogLength, nullptr, infoLog);
+          std::println("ERROR:Shader program link failed\n{}", infoLog);
+        }
+      }
+
+      // Delete shader objects if we linked them into the shader program
+      glDeleteShader(vertexShader);
+      glDeleteShader(fragmentShader);
+
+      // Active shader program
+      glUseProgram(shaderProgram);
 
       pollGLFWEvent(window);
     }
