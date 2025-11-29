e2089ebfe0f94efffad97b2dc0b8f372e1772bb1
麦 <139850260+HunterFinal@users.noreply.github.com>
2025-11-29 15:43:49 +0900

    Update to 6.1

--- src/Main.cpp
+++ src/Main.cpp
@@ -49,7 +49,7 @@ int main()
 {
   using OpenGLStudy::GL_NDCVertex;
 
-  std::print("Hello GCC!");
+  std::print("Hello GCC!Size of GL_NDCVertex is {}", sizeof(GL_NDCVertex));
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -74,7 +74,73 @@ int main()
     }
   
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  
+
+    // Initialize triangle
+
+    // Vertices
+    GL_NDCVertex vertices[] =
+    {
+      {-0.5f, -0.5f, 0.0f},
+      { 0.5f, -0.5f, 0.0f},
+      { 0.0f,  0.5f, 0.0f}
+    };
+
+    // Indices
+    uint32_t indices[] =
+    {
+      0, 1, 3,
+      1, 2, 3
+    };
+
+    // Use vertex buffer object to store vertices(OpenGL object)
+    uint32_t VBO = 0;
+    glGenBuffers(1, &VBO);
+
+    // Generate vertex array object(OpenGL object)
+    uint32_t VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // Generate element buffer object
+    uint32_t EBO;
+    glGenBuffers(1, &EBO);
+
+    
+
+    // bind vertex array object;
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    /**
+     * Copy user-defined data into the currently bound buffer
+     * First argument: Type of buffer
+     * Second argument: Size of the data we want to pass to the buffer
+     * Third argument: Actual data we want to send
+     * Fourth argument: Specifies how we want the graphics card to manage the given data
+     *                  GL_STREAM_DRAW: The data is set only once and used by the GPU at most a few times
+     *                  GL_STATIC_DRAW: The data is set only once and used many times
+     *                  GL_DYNAMIC_DRAW: The data is changed a lot and used many times
+     */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Tell OpenGL how it should interpret vertex data
+    /**
+     * // NOTE layout (location = 0)
+     * First argument: Specifies which vertex attribute we want to configure
+     * // NOTE vec3 -> 3
+     * Second argument: Size of the vertex attribute.
+     * // NOTE vec* in GLSL consists of floating point values -> GL_FLOAT
+     * Third argument: Specifies the type of data
+     * // NOTE GL_TRUE if we inputting integer data type(int, byte)
+     * Fourth argument: Specifies if we want the data to be normalized
+     * // NOTE We could've also specified the stride as 0 to let OpenGL determine it.
+     * // NOTE Only works when values are tightly packed
+     * Fifth argument: Known as the Stride.Space between consecutive vertex attributes
+     * // FIXME Explore this in more detail later
+     * Sixth argument: Offset of where the position data begins.
+     */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_NDCVertex), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+    
     while(!glfwWindowShouldClose(window))
     {
       processInput(window);
@@ -82,34 +148,6 @@ int main()
       glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      GL_NDCVertex vertices[] =
-      {
-        {-0.5f, -0.5f, 0.0f},
-        { 0.5f, -0.5f, 0.0f},
-        { 0.0f,  0.5f, 0.0f}
-      };
-
-      // Use vertex buffer object to store vertices(OpenGL object)
-      uint32_t VBO = 0;
-      glGenBuffers(1, &VBO);
-      glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-      /**
-       * Copy user-defined data into the currently bound buffer
-       * First argument: Type of buffer
-       * Second argument: Size of the data we want to pass to the buffer
-       * Third argument: Actual data we want to send
-       * Fourth argument: Specifies how we want the graphics card to manage the given data
-       *                  GL_STREAM_DRAW: The data is set only once and used by the GPU at most a few times
-       *                  GL_STATIC_DRAW: The data is set only once and used many times
-       *                  GL_DYNAMIC_DRAW: The data is changed a lot and used many times
-       */
-      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-      {
-
-      }
       constexpr int32_t infoLogLength = 512;
       // Create vertex shader object(OpenGL object)
       uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -134,7 +172,7 @@ int main()
           std::println("ERROR:Shader vertex compilation failed\n{}", infoLog);
         }
       }
-      
+
       // Create fragment shader object(OpenGL object)
       uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
       glShaderSource(fragmentShader, 1, &GLSLShaderText::fragmentShaderSource, nullptr);
@@ -177,6 +215,8 @@ int main()
 
       // Active shader program
       glUseProgram(shaderProgram);
+      glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
 
       pollGLFWEvent(window);
     }
